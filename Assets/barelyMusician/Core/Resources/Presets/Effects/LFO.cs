03c41c25a34502d1ce0d0e4ede271ddb124e8abe
Alper Gungormusler <alpynaq@gmail.com>
2014-08-19 00:25:58 +0100

    LFO added, going EXPERIMENTAL!

---
+++ Assets/barelyMusician/Core/Resources/Presets/Effects/LFO.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BarelyAPI
+{
+    public class LFO : AudioEffect
+    {
+        // Distortion level
+        private Oscillator osc;
+        public float Frequency
+        {
+            get { return osc.Frequency; }
+            set { osc.Frequency = value; }
+        }
+
+        public LFO(float frequency)
+        {
+            osc = new Oscillator(OscillatorType.SINE);
+            Frequency = frequency;
+        }
+
+        public LFO()
+            : this(5.0f)
+        {
+        }
+
+        public override void Apply(TimbreProperties timbreProperties)
+        {
+            Frequency = 10.0f * (0.5f * timbreProperties.Brightness + 0.5f * timbreProperties.Tense);
+        }
+
+        public override float Process(float sample)
+        {
+            return sample * osc.Next();
+        }
+    }
+}
---
+++ Assets/barelyMusician/Core/Resources/Presets/Generators/MicroGenerators/VelocityDrumsMicroGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BarelyAPI
+{
+    public class VelocityDrumsMicroGenerator : MicroGenerator
+    {
+
+        public VelocityDrumsMicroGenerator(Sequencer sequencer)
+            : base(sequencer)
+        {
+        }
+
+        protected override void generateLine(SectionType section, int bar, int harmonic, ref List<NoteMeta> line)
+        {
+            if (section != SectionType.INTRO && section != SectionType.OUTRO)
+            {
+                float offset = 0.0f;
+                float duration = 0.25f / LineLength;
+
+                for (int i = 0; i < LineLength * 4; ++i)
+                {
+                    // kick
+                    if (RandomNumber.NextFloat(0.0f, 1.0f) < 0.025f || (RandomNumber.NextFloat(0.0f, 1.0f) < 0.96f && i % 4 == 0))
+                        line.Add(new NoteMeta(0, offset, duration, 1.0f));
+                    // snare
+                    if ((RandomNumber.NextFloat(0.0f, 1.0f) < 0.025f || i % 8 == 4))
+                        line.Add(new NoteMeta(7, offset, duration, 1.0f));
+                    // hihat closed
+                    if (RandomNumber.NextFloat(0.0f, 1.0f) < 0.01f || i % 4 == 2)
+                        line.Add(new NoteMeta(14, offset, duration, RandomNumber.NextFloat(0.5f, 1.0f)));
+                    // hihat open
+                    if (RandomNumber.NextFloat(0.0f, 1.0f) < 0.025f)
+                        line.Add(new NoteMeta(21, offset, duration, 1.0f));
+
+                    offset += duration;
+                }
+            }
+        }
+    }
+}
